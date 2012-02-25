ce6616baef23f2fe132227a64c5bb5a2f4c3054f
Taufiq Hoven <taufiq.hoven@gmail.com>
2012-02-25 13:19:22 +1000

    Added functionality for looping through players.

--- src/xp_decay/core.clj
+++ src/xp_decay/core.clj
@@ -9,13 +9,46 @@
   (:import org.bukkit.plugin.java.JavaPlugin
            java.util.concurrent.TimeUnit)))
 
-(defn jp-init []
-  "Initialises the player list"
-  (def state {:players (ref (hash-map))}))
+(def ^{:doc "Default decrement of XP"} ^:const DEFAULT_XP_DECREMENT 10)
 
-(defn jp-onEnable [] nil)
+(def ^{:doc "Strength of the explosion that occurs when XP is zero"} DEFAULT_EXPLOSION_STRENGTH 0xFFFF)
 
-; Timer
+(def ^{:doc "Whether or not a fire should be set when the explosion occurs"} DEFAULT_SET_FIRE false)
+
+(defrecord Plugin-state [players trigger])
+
+(defn on-zero-xp! [plugin player]
+  (.createExplosion (.getWorld plugin) (.getLocation player) DEFAULT_EXPLOSION_STRENGTH DEFAULT_SET_FIRE))
+
+(defn reduce-xp! [plugin zero-xp-trigger player]
+  "Reduces xp on the specified player and calls zero-xp-trigger if that
+player's experience has fallen to zero or below. The zero-xp-trigger function
+may have side effects."
+  (let [new-xp (max 0 (- (.getExperience player) DEFAULT_XP_DECREMENT))]
+    (.setExperience player new-xp)
+    (if (= new-xp 0)
+      (zero-xp-trigger player))))
+
+(defn action-players [plugin]
+  "Actions the players in the given plugin."
+  (let [state @(.state plugin)
+        action! (partial reduce-xp! plugin (:trigger state))
+        players (:players state)]
+    (loop [player-names (keys players)]
+      (let [[first-player-name & rem-players] player-names
+            first-player (first-player-name players)]
+        (if-not (nil? (first-player))
+          (action! first-player))
+        (if-not (empty? rem-players) (recur rem-players))))))
+
 (defn add-timer [plugin]
+  "Adds a timer to the given plugin, which when called will reduce XP on that
+player and perform any additional triggered actions."
   (let [scheduler (-> plugin .getServer .getScheduler)]
-    nil))
+    (.scheduleSyncDelayedTask scheduler plugin #(action-players plugin))))
+
+(defn jp-init [this]
+  "Initialises the player list"
+  (def state (Plugin-state. (ref (hash-map)) on-zero-xp!)))
+
+(defn jp-onEnable [this] nil)
