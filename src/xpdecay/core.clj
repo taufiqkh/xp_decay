a13160650198e44b0d8ce92437fb652deefb0ed0
Taufiq Hoven <taufiq.hoven@gmail.com>
2012-03-01 01:13:24 +1000

    Fixed timer, added function to add player to list of those for which xp is reduced.

--- src/xpdecay/core.clj
+++ src/xpdecay/core.clj
@@ -1,5 +1,6 @@
 (ns xpdecay.core
   "Xp decay plugin for Bukkit")
+  ; TODO: Rewrite with less of an imperative style
 
 (def ^{:doc "Default decrement of XP"} ^:const DEFAULT_XP_DECREMENT 10)
 
@@ -11,9 +12,9 @@
 
 (def ^{:doc "Starting level for a player"} ^:const ^int DEFAULT_START_LEVEL 50)
 
-; Set to level 51 experience - 1
+; Set to level 50 experience
 (def ^{:doc "Starting experience for a player"} ^:const ^int DEFAULT_START_EXPERIENCE
-  (dec (reduce + (map xp-to-next-level (range 0 (inc DEFAULT_START_LEVEL))))))
+  (reduce + (map xp-to-next-level (range 0 DEFAULT_START_LEVEL))))
 
 (def ^{:doc "Ticks per second on an ideal server"} ^:const SERVER_TICKS_PER_SEC 20)
 
@@ -25,9 +26,11 @@
 
 (defn on-zero-xp! [plugin player]
   "Creates an explosion at the player coordinates."
-  (.createExplosion (.getWorld plugin) (.getLocation player) DEFAULT_EXPLOSION_STRENGTH DEFAULT_SET_FIRE))
+  (.sendMessage player "Boom!")
+  (.giveExp player DEFAULT_START_EXPERIENCE))
+  ;(.createExplosion (.getWorld plugin) (.getLocation player) DEFAULT_EXPLOSION_STRENGTH DEFAULT_SET_FIRE))
 
-(def state-ref (Plugin-state. (ref (hash-map)) on-zero-xp!))
+(def state-ref (ref (Plugin-state. (sorted-set) on-zero-xp!)))
 
 (defn info [msg & args]
   (.info logger (apply format msg args)))
@@ -58,30 +61,44 @@
 player's experience has fallen to zero or below. The zero-xp-trigger function
 may have side effects."
   (let [new-xp (max 0 (- (.getTotalExperience player) DEFAULT_XP_DECREMENT))]
-    (.setTotalExperience player new-xp)
+    (.giveExp player (- DEFAULT_XP_DECREMENT))
+    (info "Xp on %s reduced to %d" (.getName player) new-xp)
     (if (= new-xp 0)
-      (zero-xp-trigger player))))
+      (zero-xp-trigger plugin player))))
 
 (defn action-players [plugin]
   "Actions the players in the given plugin."
   (let [state @state-ref
-        action! (partial reduce-xp! plugin on-zero-xp!)
-        players (:players state)]
-    (loop [player-names (keys players)]
-      (let [[first-player-name & rem-players] player-names
-            first-player (first-player-name players)]
-        (if-not (nil? (first-player))
-          (action! first-player))
-        (if-not (empty? rem-players) (recur rem-players))))))
+        action! (partial reduce-xp! plugin on-zero-xp!)]
+    (info "Actioning players %s" (pr-str (:players state)))
+    (doseq [player-name (:players state)]
+      (let [player (.getPlayer (.getServer plugin) player-name)]
+        (if-not (nil? player)
+          (action! player))))))
 
-(defn add-timer [plugin]
+(defn add-timer! [plugin]
   "Adds a timer to the given plugin, which when called will reduce XP on that
 player and perform any additional triggered actions."
   (let [scheduler (-> plugin .getServer .getScheduler)]
-    (.scheduleSyncDelayedTask scheduler plugin #(action-players plugin) SERVER_TICKS_PER_SEC)))
+    (.scheduleAsyncRepeatingTask scheduler plugin #(action-players plugin) SERVER_TICKS_PER_SEC SERVER_TICKS_PER_SEC)))
+
+(defn gen-command-executor []
+  (reify org.bukkit.command.CommandExecutor
+    (onCommand [this sender command label args]
+               (if (.equals "add" (aget args 0)) ;(.hasPermission sender "xpdecay.player.add"))
+                 (let [player-name (aget args 1)]
+                   (dosync
+                     (alter state-ref assoc :players (conj (:players @state-ref) player-name)))
+                   (.sendMessage sender (format "Player %s added to the xp decay list." player-name))
+                   (info (pr-str @state-ref))
+                   (info "Sender %s added %s to the xp decay list" (.getName sender) player-name)
+                   true)
+                 (do (.sendMessage sender "Unknown subcommand") false)))))
 
 (defn enable-plugin [clj-plugin-loader]
   (info (format "Start xp: %d" DEFAULT_START_EXPERIENCE))
+  (.setExecutor (.getCommand clj-plugin-loader "xpd") (gen-command-executor))
+  (add-timer! clj-plugin-loader)
   (let [plugin-manager (-> clj-plugin-loader .getServer .getPluginManager)]
     (.registerEvents plugin-manager (XpDecayEventHandler. clj-plugin-loader) clj-plugin-loader)))
 
